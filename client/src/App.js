fd796a9f70515c7233376e820607ff141c04e57e
Sean Hopkins <seahopki12@gmail.com>
2021-01-09 13:35:48 -0500

    deleted node modules and restructured repo

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          {/* <Route exact path="/">
+            <Books />
+          </Route> */}
+          <Route exact path="/About">
+            <About />
+          </Route>
+          <Route exact path="/Contact">
+            <Contact />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
