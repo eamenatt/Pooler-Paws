d50b3a729551fa135e0fe24864fc415388c33e76
Eric M <eamenatt@gmail.com>
2021-01-10 16:48:34 -0500

    adding model for cat and user and seed file for cats. Still need to add in more validation and parameters for username and password.

---
+++ models/cat.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const catSchema = new Schema({
+  namee: { type: String, required: true },
+  age: { type: Number, required: true },
+  details: { String, required: true },
+  picture: {
+    type: String,
+    get: v => `${root}${v}`
+  },
+  availability: { type: Date, default: Date.now }
+});
+
+const Cat = mongoose.model("Cat", catSchema);
+
+const doc = new Cat({ name: 'Val', picture: '/123.png' });
+doc.picture; // 'https://s3.amazonaws.com/mybucket/123.png'
+doc.toObject({ getters: false }).picture; // '/123.png'
+
+module.exports = Cat;
