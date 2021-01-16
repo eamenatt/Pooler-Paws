bd76d526a520eef1b73b40d5daf7c523d86a1641
Sean Hopkins <seahopki12@gmail.com>
2021-01-16 14:06:55 -0500

    rendered list of cats to front end.

---
+++ client/src/utils/actions.js
@@ -0,0 +1,9 @@
+export const UPDATE_CATS = "UPDATE_CATS";
+export const REMOVE_CAT = "REMOVE_CAT";
+export const SET_CURRENT_CAT = "SET_CURRENT_CAT";
+export const ADD_CAT = "ADD_CAT";
+export const LOADING = "LOADING";
+
+export const ADD_FAVORITE = "ADD_FAVORITE";
+export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
+export const UPDATE_FAVORITES = "UPDATE_FAVORITES";
