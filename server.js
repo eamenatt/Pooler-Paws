3db036458502a4dd48e7f24409231172e69e2824
Eric Menatti <68793022+eamenatt@users.noreply.github.com>
2021-01-25 20:51:46 -0500

    fixing deployment issues and rendering of petlist page. Basically replaced entire server.js

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const cors = require("cors");
-const PORT = process.env.PORT || 3001;
-require("dotenv").config();
-//static authenticate method of model in local strategy
-
-// Define middleware here
 const app = express();
+const PORT = process.env.PORT || 3001;
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
 }
+// Add routes, both API and view
+app.use(routes);
 // Connect to the Mongo DB
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/catlist", {
@@ -31,10 +22,7 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected!"))
   .catch((error) => console.log("MongoDB did not connect: ", error));
-
-// Add routes, both API and view
-app.use(routes);
 // Start the API server
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
