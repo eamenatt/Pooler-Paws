836325dbf82da7cd7577fb3b77fddc763193a649
Eric Menatti <68793022+eamenatt@users.noreply.github.com>
2021-08-01 13:45:04 -0400

    updating list on Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -52,7 +52,7 @@ function Contact() {
       <Jumbotron>
             <h1>Contact Pooler Paws!</h1>
             <p>
-                Thank you for your visiting our site and wanting to reach out. We have many ways to actively get involved with Pooler Paws! Contact us by filling out the form below for any of the following:  
+                Thank you for your visiting our site and wanting to reach out. We have many ways to actively get involved with Pooler Paws! Please follow us on <a href="https://www.facebook.com/poolerpawsinc/">Facebook</a> and <a href="https://www.instagram.com/poolerpawsinc/?hl=en">Instagram</a>:  
             </p>
            <ul>
                <li>Socialize the cats and help clean at the Cat House (a great idea for middle and high school students looking for service hours)</li>
@@ -63,16 +63,20 @@ function Contact() {
                 Donation information: We are a 501(c)(3) nonprofit. All contributions are tax deductible.  
             </p>
            <ul>
-               <li>Paypal  (paypal.me/poolerpaws2)</li>
-               <li>Checks (made out to Pooler Paws, Inc.) Please use contact form for address to mail checks to.</li>
-                <li>Cat litter.  Clumping and multi-cat type is preferred.</li>
+                <li>Venmo @Julianne-Haynes-2</li>
+                <li>You can also visit our Amazon Wish list here: <a href="https://www.amazon.com/hz/wishlist/ls/1ZNQ7OHQHVPSX?ref_=wl_share">Pooler Paws Amazon Wish List</a></li>
+                <li>Paypal: <a href="paypal.me/poolerpaws2">paypal.me/poolerpaws2</a></li>
+           </ul>
+            <p>
+                Should you wish to donate goods directly, please use the contact form below and we will reach out to you for delivery/pick up.  Some items we are always in need of:  
+            </p>
+            <ul>
                 <li>Canned and dry cat food.  </li>
                 <li>Canned and dry kitten food.</li>
                 <li>Tall (13 gallon) Kitchen drawstring garbage bags.</li>  
-                <li>Cleaning supplies: dish soap, multipurpose cleaners, bleach, laundry detergent
-                Paper towels.</li>
+                <li>Cleaning supplies: dish soap, multipurpose cleaners, bleach, laundry detergent, paper towels.</li>
                 <li>XL Piddle Pads</li>
-           </ul>
+            </ul>
         </Jumbotron>
       <ContactForm />
     </div>
